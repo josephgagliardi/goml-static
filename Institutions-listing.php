70f17e8e4774e555b3bdc21cec33faf33f9acaf1
Jesse Brewer <jbrewer@westga.edu>
2019-02-20 16:54:01 -0500

    added logos and config layout for institutions listing/single pages

--- Institutions-listing.php
+++ Institutions-listing.php
@@ -3,70 +3,99 @@
 <?php include("includes/head.php"); ?>
 <?php include("includes/header.php"); ?>
 <main>
-    <div class="sc-partner-listing-page">
-        <div class="container">
-            <div class="partner-listing-wrapper">
-                <div class="row justify-content-center">
-                    <div class="col-6 align-self-center partner-listing-header">
-                        <h3 class="title-text">Institions</h3>
-                        <p class="content-text">Our Institions are some of the most forward-looking university in the country, and they're looking for graduates to fill critical roles today.</p>
-                       <div class="row text-center">
-                           <div class="col-6">23 <span>Institions</span></div>
-                           <div class="col-6">offering 2,615 degrees</div>
-                        </div>
+    <div class="container">
+        <div class="partner-listing-wrapper">
+            <div class="row justify-content-center">
+                <div class="col-6 align-self-center partner-listing-header">
+                    <h1 class="title-text">Institions</h1>
+                    <p class="content-text">Our Institions are some of the most forward-looking university in the country, and they're looking for graduates to fill critical roles today.</p>
+                    <div class="row text-center">
+                        <div class="col-6">23 <span>Institions</span></div>
+                        <div class="col-6">offering 2,615 degrees</div>
                     </div>
                 </div>
-                <div class="partner-listing-main">
-                    <div class="partner-listing-main__list">
-                        <div class="university-menu">
-                            <div class="row">
-                                <div class="col-4">
-                                    <a class="university-menu__item" href="#">
-                                        <div class="item-top d-flex"><img src="/assets/img/institutions/Abraham-Baldwin-Agricultural-College-COLOR.svg"></div>
-                                        <div class="item-body">
-                                            <h5>Abraham Baldwin Agricultural College</h5>
-                                        </div>
-                                    </a>
-                                </div>
-                                <div class="col-4">
-                                    <a class="university-menu__item" href="#">
-                                        <div class="item-top d-flex"><img src="/assets/img/institutions/Albany-State-COLOR.svg"></div>
-                                        <div class="item-body">
-                                            <h5>Albany State University</h5>
-                                        </div>
-                                    </a>
-                                </div>
-                                <div class="col-4">
-                                    <a class="university-menu__item" href="#">
-                                        <div class="item-top d-flex"><img src="/assets/img/institutions/atlanta-metro-color.svg"></div>
-                                        <div class="item-body">
-                                            <h5>Atlanta Metropolitan State College</h5>
-                                        </div>
-                                    </a>
-                                </div>
-                                <div class="col-4">
-                                    <a class="university-menu__item" href="#">
-                                        <div class="item-top d-flex"><img src="/assets/img/institutions/College-of-Coastal-Georgia-color.svg"></div>
-                                        <div class="item-body">
-                                            <h5>College of Coastal Georgia</h5>
-                                        </div>
-                                    </a>
-                                </div>
-
-
+            </div>
+            <div class="partner-listing-main">
+                <div class="partner-listing-main__list">
+                    <div class="university-menu">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                                <a class="university-menu__item col-equal" href="#">
+                                    <div class="item-top"><img src="/assets/img/institutions/Abraham-Baldwin-Agricultural-College-COLOR.svg" alt=""></div>
+                                    <div class="item-body">
+                                        Abraham Baldwin Agricultural College
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                                <a class="university-menu__item col-equal" href="#">
+                                    <div class="item-top"><img src="/assets/img/institutions/Albany-State-COLOR.svg" alt=""></div>
+                                    <div class="item-body">
+                                        Albany State University
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                                <a class="university-menu__item col-equal" href="#">
+                                    <div class="item-top"><img src="/assets/img/institutions/atlanta-metro-color.svg" alt=""></div>
+                                    <div class="item-body">
+                                        Atlanta Metropolitan State College
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                                <a class="university-menu__item col-equal" href="#">
+                                    <div class="item-top"><img src="/assets/img/institutions/Augusta-University-color.svg" alt=""></div>
+                                    <div class="item-body">
+                                        Atlanta Metropolitan State College
+                                    </div>
+                                </a>
+                            </div>
+                             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                                <a class="university-menu__item col-equal" href="#">
+                                    <div class="item-top"><img src="/assets/img/institutions/clayton-state-color.svg" alt=""></div>
+                                    <div class="item-body">
+                                        College of Coastal Georgia
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                                <a class="university-menu__item col-equal" href="#">
+                                    <div class="item-top"><img src="/assets/img/institutions/College-of-Coastal-Georgia-color.svg" alt=""></div>
+                                    <div class="item-body">
+                                        College of Coastal Georgia
+                                    </div>
+                                </a>
+                            </div>
+                             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                                <a class="university-menu__item col-equal" href="#">
+                                    <div class="item-top"><img src="/assets/img/institutions/columbus-state-university-color.svg" alt=""></div>
+                                    <div class="item-body">
+                                        Columbus State University
+                                    </div>
+                                </a>
+                            </div>
+                              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                                <a class="university-menu__item col-equal" href="#">
+                                    <div class="item-top"><img src="/assets/img/institutions/dalton-state-color.svg" alt=""></div>
+                                    <div class="item-body">
+                                        College of Coastal Georgia
+                                    </div>
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <nav class="breadcrumb-wapper">
-                <ul class="breadcrumb">
-                    <li class="breadcrumb-item"><a class="breadcrumb__link" href="index.html">home</a></li>
-                    <li class="breadcrumb-item active"><a class="breadcrumb__link active" href="#">Institions</a></li>
-                </ul>
-            </nav>
         </div>
+        <nav class="breadcrumb-wapper">
+            <ul class="breadcrumb">
+                <li class="breadcrumb-item"><a class="breadcrumb__link" href="index.html">home</a></li>
+                <li class="breadcrumb-item active"><a class="breadcrumb__link active" href="#">Institions</a></li>
+            </ul>
+        </nav>
     </div>
+
 </main>
 <?php include("includes/footer.php"); ?>
 
--- Institutions-single.php
+++ Institutions-single.php
@@ -1,23 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include("includes/head.php"); ?>
-<?php include("includes/header.php"); ?>   
+<?php include("includes/header.php"); ?>
 
-    <main class="partner-details">
-        <div class="container">
-            <div class="partner-header">
-                <div class="row">
-                    <div class="col-md-2">
-                        <div class="partner-header__wapper"><img class="partner-header__image" src="assets/img/partner-details/partner-header.png" alt="University of maryland"></div>
+<main class="partner-details">
+    <div class="container">
+        <div class="partner-header">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="partner-header__wapper">
+                        <img class="partner-header__image" src="/assets/img/institutions/Abraham-Baldwin-Agricultural-College-COLOR.svg" alt="">
                     </div>
-                    <div class="col-md-10">
-                        <div class="partner-header__title">University of Maryland, College Park</div>
-                        <div class="partner-header__content">The University of Maryland is the state's flagship university and one of the nation's preeminent public research universities. A global leader in research, entrepreneurship and innovation, the university is home to more than 37,000 students, 9,000 faculty and staff, and 250 academic programs. Its faculty includes three Nobel laureates, three Pulitzer Prize winners, 47 members of the national academies and scores of Fulbright scholars. The institution has a $1.8 billion operating budget, secures $500 million annually in external research funding and recently completed a $1 billion fundraising campaign.</div>
+                    <nav>
+                        <ul class="list-link__list">
+                            <li class="list-link__item"><a class="list-link__link" href="#list-link-2">Accreditation</a></li>
+                            <li class="list-link__item"><a class="list-link__link" href="#list-link-1">Online Programs</a></li>
+                            
+                        </ul>
+                    </nav>
+                    <div class="mt-4 p-3">
+                        <a class="btn button-default btn-block list-link__btn" href="#">Apply Now</a>
+                        <a class="btn button-outline-02 btn-block list-link__btn" href="#">Request Info</a>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="partner-header__title">Abraham Baldwin Agricultural College</div>
+                    <div class="partner-header__content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vulputate fringilla tellus, in lacinia eros ullamcorper eget. Nam dolor enim, tincidunt eu felis eu, lacinia pretium nisi. In sed dolor dignissim, rutrum massa ac, egestas ex. Nulla congue placerat lacus, sed dignissim leo varius at. Aenean sodales sem velit, in volutpat ligula eleifend id. In hendrerit sagittis odio ac rhoncus. Curabitur at eros quam. Donec sed turpis in risus convallis ullamcorper. Quisque ac odio sit amet nisi rhoncus luctus et vel ex. Aliquam luctus sagittis blandit. Cras vel diam ac leo sagittis finibus vel non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a justo id tortor porttitor sagittis.
+                    
                     </div>
                 </div>
             </div>
         </div>
-       
-    </main>
-   <?php include("includes/footer.php"); ?>
+    </div>
+
+</main>
+<?php include("includes/footer.php"); ?>
+
 </html>
