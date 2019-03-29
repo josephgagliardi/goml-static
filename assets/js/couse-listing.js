cffaf6786d19382cdc33bf8b5a2fe9bc39074f3c
Joseph Gagliardi <jsphgag@gmail.com>
2019-03-29 14:15:08 -0400

    reformat page layout for search page

--- assets/js/couse-listing.js
+++ assets/js/couse-listing.js
@@ -1,47 +1,47 @@
-"use strict";
-$(document).ready(function () {
-	var window_width = $(window).width(),
-		$textSelect = $('#select-text'),
-		$list = $('#list-categories');
+// "use strict";
+// $(document).ready(function () {
+// 	var window_width = $(window).width(),
+// 		$textSelect = $('#select-text'),
+// 		$list = $('#list-categories');
 
-	$list.on('click', 'a.list-categories__link', function () {
-		var text = $(this).text();
-		$textSelect.html(text);
+// 	$list.on('click', 'a.list-categories__link', function () {
+// 		var text = $(this).text();
+// 		$textSelect.html(text);
 
-		$list.find('a.list-categories__link').removeClass('active');
-		$(this).addClass('active');
+// 		$list.find('a.list-categories__link').removeClass('active');
+// 		$(this).addClass('active');
 
-		if (window_width <= 768) {
-			$('#btn-list-categories').trigger('click');
-		}
-	})
+// 		if (window_width <= 768) {
+// 			$('#btn-list-categories').trigger('click');
+// 		}
+// 	})
 
-	$('body').on('click', function (e) {
-		var $trigger = $(".list-categories");
+// 	$('body').on('click', function (e) {
+// 		var $trigger = $(".list-categories");
 
-		if ($trigger !== e.target && !$trigger.has(e.target).length) {
-			$('.list-categories').collapse('hide');
-			$('.collapse').collapse('hide');
-		}
-	});
+// 		if ($trigger !== e.target && !$trigger.has(e.target).length) {
+// 			$('.list-categories').collapse('hide');
+// 			$('.collapse').collapse('hide');
+// 		}
+// 	});
 
-	$('#list').click(function (event) {
-		event.preventDefault();
-		$('#courses .courses__item').addClass('list-view');
-		$('#courses .courses__item').removeClass('grid-group-item');
-		$('.col-equal').matchHeight({
-			remove: true
-		});
-	});
-	$('#grid').click(function (event) {
-		event.preventDefault();
-		$('#courses .courses__item').removeClass('list-view');
-		$('#courses .courses__item').addClass('grid-group-item');
-		$('.col-equal').matchHeight({
-			remove: false
-		});
-	});
+// 	$('#list').click(function (event) {
+// 		event.preventDefault();
+// 		$('#courses .courses__item').addClass('list-view');
+// 		$('#courses .courses__item').removeClass('grid-group-item');
+// 		$('.col-equal').matchHeight({
+// 			remove: true
+// 		});
+// 	});
+// 	$('#grid').click(function (event) {
+// 		event.preventDefault();
+// 		$('#courses .courses__item').removeClass('list-view');
+// 		$('#courses .courses__item').addClass('grid-group-item');
+// 		$('.col-equal').matchHeight({
+// 			remove: false
+// 		});
+// 	});
 
 
 
-});
+// });
--- search.js
+++ search.js
@@ -12,11 +12,16 @@ const search = instantsearch({
 
 search.addWidget(
   instantsearch.widgets.searchBox({
-    container: '.form-search__input-group',
+    container: '#searchBox',
     showSubmit: true,
-    showReset: true,
+    showReset: false,
     searchAsYouType: true,
-    placeholder: 'Your future is merely a search away...'
+    placeholder: 'Your future is merely a search away...',
+    cssClasses: {
+      item: 'form-search__input-group',
+      input: 'form-control form-search__input',
+      submit: 'btn form-search__button'
+    }
   })
 );
 
@@ -28,125 +33,57 @@ search.addWidget(
       item: function(item) {
       cpl = item["credit for prior learning"] === "TRUE" ? "inherit" : "none";
       return `
-         <div class="courses__wrapper">
             <div class="courses__top">
-               <div class="courses__top__wrapper"><img class="courses__top__image" src="assets/img/course-single/header-img-1.jpg" alt=""></div>
+               <div class="courses__top__wrapper"><img class="courses__top__image" src="${item["logo"]}" alt=""></div>
             </div>
-            <div class="courses__content h-100">
-               <a href="course-single.php" class="courses__content__title">Master of Education in Early Childhood Education</a>
+            <div class="courses__content">
+               <a class="courses__content__title" href="#">${item["program title"]}</a>
                <div class="courses__bottom">
-                  <a class="courses__col-left" href="#"><span class="courses__content__institutions">University Of West Georgia</span></a>
-                  <ul class="list-tags">
+                  <a class="courses__col-left" href="#"><span class="courses__content__institutions">${item["area of study"]}</span></a>
+                  <ul class="list-tags pb-1">
                      <li>
-                        <button class="aos" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Area of Study" data-original-title="" title="">aos</button>
+                        <button class="aos" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Area of Study" data-original-title="" title="">${item["area of study"]}</button>
                      </li>
-                     <li>
+                     <li style="display: ${cpl}">
                         <button class="cpl" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Credit for Prior Learning" data-original-title="" title="">cpl</button>
                      </li>
                      <li>
-                        <button class="ep" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="eMajor program" data-original-title="" title="">em</button>
+                        <button class="ep" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="eMajor program" data-original-title="" title="">${item["total hours"]}</button>
                      </li>
                   </ul>
                </div>
             </div>
-            <div class="courses__icon bt-circle-fill-red"><i class="fas fa-book-reader"></i></div>`;
-      // return `
-      // <div class="col-lg-4 col-md-4 col-sm-6 courses__item">
-      // <div class="courses__wrapper h-100">
-      //       <div class="courses__top">
-      //          <div class="courses__top__wrapper"><img class="courses__top__image" src="${item["logo"]}" alt=""></div>
-      //       </div>
-      //       <div class="courses__content">
-      //          <a class="courses__content__title" href="#">${item["program title"]}</a>
-      //          <div class="courses__bottom">
-      //             <a class="courses__col-left" href="#"><span class="courses__content__institutions">${item["area of study"]}</span></a>
-      //             <ul class="list-tags pb-1">
-      //                <li>
-      //                   <button class="aos" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Area of Study" data-original-title="" title="">${item["area of study"]}</button>
-      //                </li>
-      //                <li style="display: ${cpl}">
-      //                   <button class="cpl" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Credit for Prior Learning" data-original-title="" title="">cpl</button>
-      //                </li>
-      //                <li>
-      //                   <button class="ep" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="eMajor program" data-original-title="" title="">${item["total hours"]}</button>
-      //                </li>
-      //             </ul>
-      //          </div>
-      //       </div>
-      //       <div class="courses__icon bt-circle-fill-red"><i class="fas fa-flask"></i></div>
-      //    </div></div>`;
-    },
+            <div class="courses__icon bt-circle-fill-red"><i class="fas fa-flask"></i></div>`;
+      },
     },
     cssClasses: {
-      item: 'col-lg-4 col-md-6 col-sm-6 courses__item'
-      // item: 'col-lg-3 col-md-4 col-sm-6 courses__item'
+      list: 'row',
+      item: '"col-lg-3 col-md-3 col-sm-6 courses__item'
     },
   })
 );
 
 search.addWidget(
   instantsearch.widgets.pagination({
-    container: '.mx-auto',
+    container: '.pagination',
     showNext: true,
-    showPrevious: true,
-    padding: 2,
+    showPrevious: false,
+    showFirst: false,
     scrollTo: 'header',
     cssClasses: {
       root: 'pagination mx-auto',
       list: 'pagination pagination__list text-center',
       item: 'page-item pagination__item',
       link: 'page-link pagination__link',
-      previousPageItem: 'pagination__item',
-      nextPageItem: 'pagination__item'
+      pageItem: 'pagination__item'
     }
   })
 );
 
-// See documentation: https://community.algolia.com/instantsearch.js/v2/widgets/refinementList.html
-// search.addWidget(
-//   instantsearch.widgets.refinementList({
-//     container: '#filter2',
-//     attribute: 'university',
-//     operator: 'or',
-//     limit: 4,
-//     showMore: true,
-//     cssClasses: {
-//       list: 'select__list',
-//       checkbox: 'select__checkmark',
-//       item: 'select__item select',
-//       // count: 'badge pull-right',
-//       active: 'active',
-//       // root: 'select__item',
-//       root: 'column-left__item',
-//       label: 'label',
-//       count: 'select__number'
-//     }
-//   })
-// );
-
-// search.addWidget(
-//   instantsearch.widgets.refinementList({
-//     container: '#level__filter',
-//     attribute: 'program level',
-//     operator: 'or',
-//     limit: 4,
-//     showMore: true,
-//     cssClasses: {
-//       list: 'select__list',
-//       item: 'select__item',
-//       // count: 'badge pull-right',
-//       active: 'active',
-//       // root: 'select__item',
-//       root: 'column-left__item',
-//       label: 'label',
-//       count: 'select__number'
-//     }
-//   })
-// );
 
 search.addWidget(
   instantsearch.widgets.refinementList({
-    container: '#filter1',
+    container: '#level__filter',
     attribute: 'program level',
     operator: 'or',
     limit: 5,
@@ -168,106 +105,96 @@ search.addWidget(
   })
 );
 
-// search.addWidget(
-//   instantsearch.widgets.refinementList({
-//     container: '#aos__filter',
-//     attribute: 'area of study',
-//     operator: 'or',
-//     limit: 5,
-//     showMore: true,
-//     // searchable: true,
-//     searchablePlaceholder: 'Refine by Discipline...',
-//     sortBy: ['name:asc'],
-//     cssClasses: {
-//       item: 'list-categories__item',
-//       count: 'badge pull-right select__number',
-//       active: 'active',
-//       label: 'list-categories__link',
-//       checkbox: 'filter__checkbox'
-//     }
-//   })
-// );
+search.addWidget(
+  instantsearch.widgets.refinementList({
+    container: '#aos__filter',
+    attribute: 'area of study',
+    operator: 'or',
+    limit: 5,
+    showMore: true,
+    searchablePlaceholder: 'Refine by Discipline...',
+    sortBy: ['name:asc'],
+    cssClasses: {
+      item: 'list-categories__item',
+      count: 'badge pull-right select__number',
+      active: 'active',
+      label: 'list-categories__link',
+      checkbox: 'filter__checkbox'
+    }
+  })
+);
 
-// search.addWidget(
-//   instantsearch.widgets.refinementList({
-//     container: '#institution__filter',
-//     attribute: 'university',
-//     operator: 'or',
-//     limit: 5,
-//     showMore: true,
-//     // searchable: true,
-//     searchablePlaceholder: 'Choose School',
-//     sortBy: ['name:asc'],
-//     cssClasses: {
-//       item: 'list-categories__item',
-//       count: 'badge pull-right',
-//       active: 'active',
-//       label: 'list-categories__link',
-//       checkbox: 'checkbox'
-//       // showMore: 'button-text'
-//     }
-//   })
-// );
+search.addWidget(
+  instantsearch.widgets.refinementList({
+    container: '#institution__filter',
+    attribute: 'university',
+    operator: 'or',
+    limit: 5,
+    showMore: true,
+    searchablePlaceholder: 'Choose School',
+    sortBy: ['name:asc'],
+    cssClasses: {
+      list: 'select__list',
+      item: 'select__item',
+      count: 'select__number',
+      active: 'active',
+      label: 'list-categories__link',
+      checkbox: 'filter__checkbox',
+      showMore: 'see-more-button-text'
+    }
+  })
+);
 
-// search.addWidget(
-//   instantsearch.widgets.refinementList({
-//     container: '#hours__filter',
-//     attribute: 'total hours',
-//     operator: 'or',
-//     limit: 5,
-//     showMore: true,
-//     cssClasses: {
-//       item: 'list-categories__item',
-//       count: 'badge pull-right',
-//       active: 'active',
-//       label: 'list-categories__link',
-//       checkbox: 'checkbox'
-//     }
-//   })
-// );
-
-// search.addWidget(
-//   instantsearch.widgets.stats({
-//     container: '.header-filter__result',
-//     cssClasses: {
-//       body: 'header-filter__number'
-//     }
-//   })
-// );
+search.addWidget(
+  instantsearch.widgets.stats({
+    container: '.header-filter__number',
+  })
+);
 
 search.addWidget(
   instantsearch.widgets.clearRefinements({
-    container: '.tags',
+    container: '.clear__filters',
     cssClasses: {
       button: 'button-outline tags__item clear__button tags__item'
     },
     templates: {
-      resetLabel: 'Clear All'
-      // item: `<div class="tags__item"><span class="button-outline">Clear All</span></div>`,
+      resetLabel: 'Clear All',
+      item: `<div class="tags__item"><span class="button-outline">Clear All</span></div>`
     },
   })
 );
 
-// search.addWidget(
-//   instantsearch.widgets.toggleRefinement({
-//     container: '#cpl__filter',
-//     attribute: 'credit for prior learning',
-//     templates: {
-//       labelText: 'CPL',
-//     },
-//   })
-// );
-// search.addWidget(
-//   instantsearch.widgets.currentRefinements({
-//     container: '.tags',
-//     showMore: true,
-//     padding: 20,
-//     cssClasses: {
-//       item: 'tags__item button-outline',
-//       label: 'tags__text',
-//     },
-//   })
-// );
+search.addWidget(
+  instantsearch.widgets.toggleRefinement({
+    container: '#cpl__filter',
+    attribute: 'credit for prior learning',
+    templates: {
+      labelText: 'CPL',
+    },
+    cssClasses: {
+      list: 'select__list',
+      item: 'select__item',
+      count: 'select__number',
+      active: 'active',
+      label: 'list-categories__link',
+      checkbox: 'filter__checkbox',
+      showMore: 'see-more-button-text'
+    },
+  })
+);
+search.addWidget(
+  instantsearch.widgets.currentRefinements({
+    container: '.tags',
+    cssClasses: {
+      // item: 'badge',
+      label: 'tags__item',
+      delete: 'filter__delete far fa-times-circle tags__icon',
+      categoryLabel: 'tags__text',
+      category: 'tags__text',
+    },
+  })
+);
+
 // search.addWidget(
 //   instantsearch.widgets.rangeSlider({
 //     container: '#hours__filter',
