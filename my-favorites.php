5dfa3b55d80001b78c1cb5622ee0ab20fd9294b2
Joseph Gagliardi <jsphgag@gmail.com>
2019-04-03 00:40:03 -0400

    Updates for favoriting functionality

---
+++ my-favorites.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("includes/head.php"); ?>
+<?php include("includes/header.php"); ?>
+<main>
+   <div class="courseListing">
+      <div class="container">
+         <div class="row">
+
+            <!-- Filters & Search Menu -->
+
+
+               <!-- Search Results Section -->
+               <div id="courses" class="row"></div>
+
+               <!-- Pagination -->
+               <div class="pagination" width="200"><s/div>
+
+            </div>
+         </div>
+
+      </div>
+   </div>
+</main> <?php include("includes/footer.php"); ?> 
+<!-- <script src="/assets/js/couse-listing.js">
+</script> -->
+<script src="../../search-favorites.js"></script>
+
+</html>
