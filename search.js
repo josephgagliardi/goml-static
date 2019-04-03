023010196e653c8aa84d542a4fdd02d8c4db5217
Joseph Gagliardi <jsphgag@gmail.com>
2019-04-03 01:27:29 -0400

    Remove old func

--- search.js
+++ search.js
@@ -93,31 +93,6 @@ function abbrv(str) {
     });
 };
 
-// function abbreviate(str, max, suffix)
-// {
-//   if((str = str.replace(/^\s+|\s+$/g, '').replace(/[\r\n]*\s*[\r\n]+/g, ' ').replace(/[ \t]+/g, ' ')).length <= max)
-//   {
-//     return str;
-//   }
-  
-//   var 
-//   abbr = '',
-//   str = str.split(' '),
-//   suffix = (typeof suffix !== 'undefined' ? suffix : ' ...'),
-//   max = (max - suffix.length);
-  
-//   for(var len = str.length, i = 0; i < len; i ++)
-//   {
-//     if((abbr + str[i]).length < max)
-//     {
-//       abbr += str[i] + ' ';
-//     }
-//     else { break; }
-//   }
-
-//   return abbr.replace(/[ ]$/g, '') + suffix;
-// };
-
 function itemFavorited(id){
   return !(localStorage.getItem(id) == null);
 };
