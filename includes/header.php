cd67af41a36a27fb1e500d05976bb4faa8cc3231
Jesse Brewer <jbrewer@westga.edu>
2019-06-14 12:59:10 -0400

    removed ecam logo. added goml svg logo.

--- includes/header.php
+++ includes/header.php
@@ -1,71 +1,51 @@
 <header role="banner">
-	<div class="header-top">
-		<div class="container">
-			<div class="row d-flex justify-content-between py-3">
-				<div class="logo d-flex align-items-center">
-					<a href="/"><img src="/assets/img/logo/goml-refresh-logo.png" alt="usg goml logo"></a>					
-					
-				</div>
-				<div class="droopmenu-open-search d-flex align-items-center">
-					<button class="button-outline">Search<i class="fas fa-search"></i></button>
-				</div>
-			</div>
-			<div>
-			</div><!-- droopmenu-extra -->
-		</div><!-- droopmenu-inner -->
-		<div class="dm-modal-search dm-search-slidedown">
-			<div class="dm-search-wrapper">
-				<div class="dm-modal-form">
-					<form id="search-form">
-						<input type="text" name="q" id="program-search" placeholder="Search...">
-						<label for="q"></label>
-						<button type="submit">
-							<i class="far fa-arrow-alt-circle-right"></i><span class="sr-only">Perform Search</span>
-						</button>
-					</form>
-				</div>
-			</div>
-			<div class="dm-search-overlay"></div>
-			<button class="dm-search-close">
-				<i class="fas fa-times"></i><span class="sr-only">Close Search</span>
-			</button>
-		</div><!-- dm-modal-search -->
-	</div>
+  <div class="header-top">
+    <div class="container">
+      <div class="row d-flex justify-content-between py-3">
+        <div class="logo d-flex align-items-center">
+          <a href="/"><img src="/assets/img/logo/goml-logo.svg" alt="goml logo and return to home page"></a>
+
+        </div>
+        <div class="droopmenu-open-search d-flex align-items-center">
+          <button class="button-outline">Search<i class="fas fa-search"></i></button>
+        </div>
+      </div>
+      <div>
+      </div><!-- droopmenu-extra -->
+    </div><!-- droopmenu-inner -->
+    <div class="dm-modal-search dm-search-slidedown">
+      <div class="dm-search-wrapper">
+        <div class="dm-modal-form">
+          <form id="search-form">
+            <input type="text" name="q" id="program-search" placeholder="Search...">
+            <label for="q"></label>
+            <button type="submit">
+              <i class="far fa-arrow-alt-circle-right"></i><span class="sr-only">Perform Search</span>
+            </button>
+          </form>
+        </div>
+      </div>
+      <div class="dm-search-overlay"></div>
+      <button class="dm-search-close">
+        <i class="fas fa-times"></i><span class="sr-only">Close Search</span>
+      </button>
+    </div><!-- dm-modal-search -->
+  </div>
 </header>
 <nav class="droopmenu-navbar">
-	<div class="droopmenu-inner">
-		<div class="droopmenu-header">
-			<a href="#" role="button" class="droopmenu-toggle">
-				<span>Menu</span>
-			</a>
-		</div><!-- droopmenu-header -->
-		<div class="droopmenu-nav">
-			<ul class="droopmenu">
-				<li><a href="/index.php">Home</a></li>
-				<li><a href="/course-listing.php">Degrees & Programs</a></li>
-				<li><a href="/Institutions-listing.php">Institutions</a></li>
-				<li>
-					<a href="#">Future Students</a>
-					<ul class="droopmenu-parent">
-						<li><a href="#">Military</a></li>
-						<li><a href="#">For Employers</a></li>
-						<li><a href="/faq.php">FAQ</a></li>
-					</ul>
-				</li>
-				<li>
-					<a href="#">About</a>
-					<ul class="droopmenu-parent">
-						<li><a href="#">eCore</a></li>
-						<li><a href="#">eMajor</a></li>
-						<li><a href="#">Staff</a></li>
-						<li><a href="#">Style Guide</a></li>
-					</ul>
-				</li>
-				<li>
-					<a href="/my-favorites.php" style="color: red;">My Favorites</a>
-				</li>
-				<li><a href="/blog-grid.php">Blog</a></li>
-			</ul>
-		</div>
-	</div>
+  <div class="droopmenu-inner">
+    <div class="droopmenu-header">
+      <a href="#" role="button" class="droopmenu-toggle">
+        <span>Menu</span>
+      </a>
+    </div><!-- droopmenu-header -->
+    <div class="droopmenu-nav">
+      <ul class="droopmenu">
+        <li><a href="/index.php">Home</a></li>
+        <li><a href="/course-listing.php">Degrees & Programs</a></li>
+        <li><a href="/Institutions-listing.php">Institutions</a></li>
+        <li><a href="/blog-grid.php">Blog</a></li>
+      </ul>
+    </div>
+  </div>
 </nav>
