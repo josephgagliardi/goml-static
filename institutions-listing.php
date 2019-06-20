bcda3981f61515a774efe2d9f3a7a25166bfbfe3
Jesse Brewer <jbrewer@westga.edu>
2019-06-20 16:35:59 -0400

    fixed file names

---
+++ institutions-listing.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("includes/head.php"); ?>
+<?php include("includes/header.php"); ?>
+<main>
+  <div class="container">
+    <div class="inst-main">
+      <div class="row justify-content-center">
+        <div class="col-6 align-self-center inst-listing-header text-center">
+          <h1 class="title-text">Institutions</h1>
+          <p class="content-text">Our institutions are some of the most forward-looking university in the country, and they're looking for graduates to fill critical roles today.</p>
+        </div>
+      </div>
+      <div class="partner-listing-main">
+        <div class="partner-listing-main__list">
+          <div class="university-menu">
+            <div class="row">
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="/institutions-single.php">
+                  <div class="item-top"><img src="/assets/img/institutions/abraham-baldwin-agricultural-college.svg" alt=""></div>
+                  <div class="item-body">
+                    Abraham Baldwin Agricultural College
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="#">
+                  <div class="item-top"><img src="/assets/img/institutions/albany-state.svg" alt=""></div>
+                  <div class="item-body">
+                    Albany State University
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="#">
+                  <div class="item-top"><img src="/assets/img/institutions/atlanta-metro.svg" alt=""></div>
+                  <div class="item-body">
+                    Atlanta Metropolitan State College
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="#">
+                  <div class="item-top"><img src="/assets/img/institutions/augusta-university.svg" alt=""></div>
+                  <div class="item-body">
+                    Atlanta Metropolitan State College
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="#">
+                  <div class="item-top"><img src="/assets/img/institutions/clayton-state.svg" alt=""></div>
+                  <div class="item-body">
+                    College of Coastal Georgia
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="#">
+                  <div class="item-top"><img src="/assets/img/institutions/college-of-coastal-georgia.svg" alt=""></div>
+                  <div class="item-body">
+                    College of Coastal Georgia
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="#">
+                  <div class="item-top"><img src="/assets/img/institutions/columbus-state-university.svg" alt=""></div>
+                  <div class="item-body">
+                    Columbus State University
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="#">
+                  <div class="item-top"><img src="/assets/img/institutions/dalton-state.svg" alt=""></div>
+                  <div class="item-body">
+                    Dalton State College
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="#">
+                  <div class="item-top"><img src="/assets/img/institutions/east-georgia-state-college.svg" alt=""></div>
+                  <div class="item-body">
+                    East Georgia State College
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="#">
+                  <div class="item-top"><img src="/assets/img/institutions/fort-valley-state-university.svg" alt=""></div>
+                  <div class="item-body">
+                    Fort Valley State University
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="#">
+                  <div class="item-top"><img src="/assets/img/institutions/georgia-college.svg" alt=""></div>
+                  <div class="item-body">
+                    Georgia College
+                  </div>
+                </a>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="university-menu__item col-equal" href="#">
+                  <div class="item-top"><img src="/assets/img/institutions/georgia-gwinnett-college.svg" alt=""></div>
+                  <div class="item-body">
+                    Georgia Gwinnett College
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <nav class="breadcrumb-wrapper">
+      <ul class="breadcrumb">
+        <li class="breadcrumb-item"><a class="breadcrumb__link" href="index.html">home</a></li>
+        <li class="breadcrumb-item active"><a class="breadcrumb__link active" href="#">Institutions</a></li>
+      </ul>
+    </nav>
+  </div>
+
+</main>
+<?php include("includes/footer.php"); ?>
+
+</html>
---
+++ institutions-single.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("includes/head.php"); ?>
+<?php include("includes/header.php"); ?>
+<main>
+  <div class="container">
+    <div class="sticky__wrapper">
+      <div class="row">
+        <div class="col-md-3">
+          <div id="menu-left">
+            <div class="list-link">
+              <div class="inst-main__img">
+                <img src="/assets/img/institutions/abraham-baldwin-agricultural-college.svg" alt="">
+              </div>
+              <nav>
+                <ul class="list-link__list">
+                  <li class="list-link__item"><a class="list-link__link" href="#list-link-1">Accreditation</a></li>
+                  <li class="list-link__item"><a class="list-link__link" href="#list-link-2">Online Programs</a></li>
+
+                </ul>
+              </nav>
+              <div class="mt-4 p-3">
+                <a class="btn button-default btn-block list-link__btn" href="#">Apply Now</a>
+                <a class="btn button-outline-02 btn-block list-link__btn" href="#">Request Info</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-9">
+          <div class="inst-main">
+            <h1 class="inst-main__title">Abraham Baldwin Agricultural College</h1>
+            <div class="inst-main__content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vulputate fringilla tellus, in lacinia eros ullamcorper eget. Nam dolor enim, tincidunt eu felis eu, lacinia pretium nisi. In sed dolor dignissim, rutrum massa ac, egestas ex. Nulla congue placerat lacus, sed dignissim leo varius at. Aenean sodales sem velit, in volutpat ligula eleifend id. In hendrerit sagittis odio ac rhoncus. Curabitur at eros quam. Donec sed turpis in risus convallis ullamcorper. Quisque ac odio sit amet nisi rhoncus luctus et vel ex. Aliquam luctus sagittis blandit. Cras vel diam ac leo sagittis finibus vel non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a justo id tortor porttitor sagittis.
+            </div>
+            <div class="bundle" id="list-link-1">
+              <h2 class="bundle__title">Accreditation</h2>
+
+              <div class="accordion" id="accordion">
+                <div class="panel accordion__item">
+                  <div class="accordion__heading">
+                    <div class="accordion__title"><button class="accordion__link btn-block" data-toggle="collapse" data-target="#answer1" aria-expanded="true">Institutionl Accreditation<i class="fas fa-angle-right accordion__icon"></i></button>
+                    </div>
+                  </div>
+                  <div class="panel-collapse collapse show" id="answer1">
+                    <div class="accordion__body">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vulputate fringilla tellus, in lacinia eros ullamcorper eget. Nam dolor enim, tincidunt eu felis eu, lacinia pretium nisi. In sed dolor dignissim, rutrum massa ac, egestas ex. Nulla congue placerat lacus, sed dignissim leo varius at. Aenean sodales sem velit, in volutpat ligula eleifend id. In hendrerit sagittis odio ac rhoncus. Curabitur at eros quam. Donec sed turpis in risus convallis ullamcorper. Quisque ac odio sit amet nisi rhoncus luctus et vel ex. Aliquam luctus sagittis blandit. Cras vel diam ac leo sagittis finibus vel non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a justo id tortor porttitor sagittis.
+                    </div>
+                  </div>
+                </div>
+                <div class="panel accordion__item">
+                  <div class="accordion__heading">
+                    <div class="accordion__title"><button class="accordion__link btn-block" data-toggle="collapse" data-target="#answer2">Program Accreditation<i class="fas fa-angle-right accordion__icon"></i></button>
+                    </div>
+                  </div>
+                  <div class="panel-collapse collapse " id="answer2">
+                    <div class="accordion__body">
+                      <ul class="accred-list">
+                        <li><img src="/assets/img/accreditation-logos/aacsb-seeklogo.com.svg"></li>
+                        <li><img src="/assets/img/accreditation-logos/ACBSP_Accredited.svg"></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="bundle-list">
+              <div class="bundle" id="list-link-2">
+                <div class="clearfix">
+                  <h2 class="bundle__title float-left">Online Programs</h2>
+                  <div class="float-right mt-md-2">
+                    <form class="form-search__expand">
+                      <label for="Search" class="sr-only">Search Terms</label>
+                      <input type="search" name="search" id="search" placeholder="Search">
+                      <em><i class="fas fa-search"></i></em>
+                    </form>
+                  </div>
+                </div>
+                <div class="bundle__content">
+                  <div class="view-more shadow">
+                    <div class="list-brief-content">
+                      <div class="list-brief-content__item">
+                        <div class="list-brief__top row">
+                          <div class="list-brief__top__title col-9 text-truncate"><a href="#">Master of Education in Early Childhood Education</a></div>
+                          <div class="col-3">
+                            <ul class="list-tags">
+                              <li>
+                                <button class="aos" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Area of Study" data-original-title="" title="">aos</button>
+                              </li>
+                              <li>
+                                <button class="cpl" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Credit for Prior Learning" data-original-title="" title="">cpl</button>
+                              </li>
+                              <li>
+                                <button class="ep" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="eMajor program" data-original-title="" title="">em</button>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="list-brief-content__item">
+                        <div class="list-brief__top row">
+                          <div class="list-brief__top__title col-9 text-truncate"><a href="#">Bachelor of Science in Information Technology</a></div>
+                          <div class="col-3">
+                            <ul class="list-tags">
+                              <li>
+                                <button class="aos" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Area of Study" data-original-title="" title="">aos</button>
+                              </li>
+                              <li>
+                                <button class="cpl" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Credit for Prior Learning" data-original-title="" title="">cpl</button>
+                              </li>
+                              <li>
+                                <button class="ep" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="eMajor program" data-original-title="" title="">em</button>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="list-brief-content__item">
+                        <div class="list-brief__top row">
+                          <div class="list-brief__top__title col-9 text-truncate"><a href="#">Associate of Science in Financial Technology (Fintech)</a></div>
+                          <div class="col-3">
+                            <ul class="list-tags">
+                              <li>
+                                <button class="aos" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Area of Study" data-original-title="" title="">aos</button>
+                              </li>
+                              <li>
+                                <button class="cpl" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Credit for Prior Learning" data-original-title="" title="">cpl</button>
+                              </li>
+                              <li>
+                                <button class="ep" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="eMajor program" data-original-title="" title="">em</button>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="list-brief-content__item">
+                        <div class="list-brief__top row">
+                          <div class="list-brief__top__title col-9 text-truncate"><a href="#">Master of Education in Early Childhood Education</a></div>
+                          <div class="col-3">
+                            <ul class="list-tags">
+                              <li>
+                                <button class="aos" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Area of Study" data-original-title="" title="">aos</button>
+                              </li>
+                              <li>
+                                <button class="cpl" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Credit for Prior Learning" data-original-title="" title="">cpl</button>
+                              </li>
+                              <li>
+                                <button class="ep" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="eMajor program" data-original-title="" title="">em</button>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="list-brief-content__item">
+                        <div class="list-brief__top row">
+                          <div class="list-brief__top__title col-9 text-truncate"><a href="#">Bachelor of Science in Information Technology</a></div>
+                          <div class="col-3">
+                            <ul class="list-tags">
+                              <li>
+                                <button class="aos" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Area of Study" data-original-title="" title="">aos</button>
+                              </li>
+                              <li>
+                                <button class="cpl" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Credit for Prior Learning" data-original-title="" title="">cpl</button>
+                              </li>
+                              <li>
+                                <button class="ep" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="eMajor program" data-original-title="" title="">em</button>
+                              </li>
+                              <li>
+                                <button class="aos" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Area of Study" data-original-title="" title="">aos</button>
+                              </li>
+                              <li>
+                                <button class="cpl" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Credit for Prior Learning" data-original-title="" title="">cpl</button>
+                              </li>
+                              <li>
+                                <button class="ep" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="eMajor program" data-original-title="" title="">em</button>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="list-brief-content__item">
+                        <div class="list-brief__top row">
+                          <div class="list-brief__top__title col-9 text-truncate"><a href="#">Associate of Science in Financial Technology (Fintech)</a></div>
+                          <div class="col-3">
+                            <ul class="list-tags">
+                              <li>
+                                <button class="aos" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Area of Study" data-original-title="" title="">aos</button>
+                              </li>
+                              <li>
+                                <button class="cpl" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="Credit for Prior Learning" data-original-title="" title="">cpl</button>
+                              </li>
+                              <li>
+                                <button class="ep" type="button" data-container="body" data-trigger="hover click" data-toggle="popover" data-placement="top" data-content="eMajor program" data-original-title="" title="">em</button>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+
+                  <button class="btn button-outline-02 btn-block list-link__btn" id="button-view">View more</button>
+
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <nav class="breadcrumb-wrapper">
+      <ul class="breadcrumb">
+        <li class="breadcrumb-item"><a class="breadcrumb__link" href="index.html">home</a></li>
+        <li class="breadcrumb-item active"><a class="breadcrumb__link" href="#">Institutions</a></li>
+        <li class="breadcrumb-item active"><a class="breadcrumb__link active" href="#">Abraham Baldwin Agricultural College</a></li>
+      </ul>
+    </nav>
+  </div>
+
+
+
+</main>
+<?php include("includes/footer.php"); ?>
+<script src="/assets/js/program-single.js"></script>
+
+</html>
