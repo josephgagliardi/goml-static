7b90ebe17ea51b83ebd2fb752e6e0138c9501fe6
Joseph Gagliardi <jsphgag@gmail.com>
2019-06-27 17:43:26 -0400

    Power homepage search, cleanup

--- homepage-search.js
+++ homepage-search.js
@@ -5,57 +5,20 @@ const searchClient = algoliasearch(
   '133c145ebb78c84a04aefb61c32dba1d'
 );
 
-
-const autocomplete = instantsearch.connectors.connectAutocomplete(
-  ({
-    indices,
-    refine,
-    widgetParams
-  }, isFirstRendering) => {
-    const {
-      container,
-      onSelectChange
-    } = widgetParams;
-
-    if (isFirstRendering) {
-      container.html('<select id="ais-autocomplete"></select>');
-
-      container.find('input').selectize({
-        options: [],
-        valueField: 'Program Name',
-        labelField: 'Program Name',
-        highlight: false,
-        onType: refine,
-        onBlur() {
-          refine(this.getValue());
-        },
-        score() {
-          return function () {
-            return 1;
-          };
-        },
-        onChange(value) {
-          onSelectChange(value);
-          refine(value);
-        },
-      });
-
-      return;
-    }
-
-    const [select] = container.find('select');
-
-    // indices.forEach(index => {
-    //   select.selectize.clearOptions();
-    //   index.results.hits.forEach(h => select.selectize.addOption(h));
-    //   select.selectize.refreshOptions(select.selectize.isOpen);
-    // });
-  }
-);
-
 const search = instantsearch({
   indexName: 'goml_DEMO',
-  searchClient
+  searchClient,
+  searchFunction(helper) {
+      const container = document.querySelector('.hit-container');
+
+      if (helper.state.query === '') {
+        container.style.display = 'none';
+      } else {
+        container.style.display = '';
+      }
+
+      helper.search();
+    }
 });
 
 search.addWidget(
@@ -74,19 +37,26 @@ search.addWidget(
   })
 );
 
-
 search.addWidget(
   instantsearch.widgets.hits({
     container: '.hit-container',
     templates: {
+      empty: '<p class="info">No results were found with your current filters. Try to remove some filters or change the search query.</p>',
       item: function (item) {
+        
+        var instLink = `<a href="/program-listing.php?query="${item['Institution']}>`;
+        var programLink = `<a href="/program-single.php?id=${item['objectID']}">`;
         return `
         <div class="inst-list-wrapper">
         <figure>
+        ${instLink}
             <img class="inst-icon" src="/assets/img/institutions/icons/${item['Institution'].replace(/\s+/g, '-').toLowerCase()}.svg" alt="">
+        </a>
             <figcaption>
-            <p class="inst">${item.Institution}</p>
-            <p class="prog-name">${item['Program Name']}</p>
+            ${programLink}
+            <p class="inst">${item['Program Name']}</p></a>
+            ${instLink}
+            <p class="prog-name">${item.Institution}</p></a>
             </figcaption>
         </figure>
         </div>
@@ -96,131 +66,6 @@ search.addWidget(
   })
 );
 
-// search.addWidget(
-//   autocomplete({
-//     container: $('.list-icons'),
-//     onSelectChange(value) {
-//       search.helper.setQuery(value).search();
-//     },
-//   })
-// );
-
-
-// search.addWidget(
-//   instantsearch.widgets.refinementList({
-//     container: '#selectLevel',
-//     attribute: 'Degree Level',
-//     operator: 'or',
-//     limit: 5,
-//     showMore: true,
-//     searchForFacetValues: true,
-//     // searchable: true,
-//     searchablePlaceholder: 'Refine by Level...',
-//     sortBy: ['name:asc'],
-//     cssClasses: {
-//       item: '',
-//     }
-//   })
-// );
 
 search.start();
 
-
-
-/* global $ instantsearch algoliasearch */
-
-
-// const autocomplete = instantsearch.connectors.connectAutocomplete(
-//   ({ indices, refine, widgetParams }, isFirstRendering) => {
-//     const { container, onSelectChange } = widgetParams;
-
-//     if (isFirstRendering) {
-//       container.html('<select id="ais-autocomplete"></select>');
-
-//       container.find('select').selectize({
-//         options: [],
-//         valueField: 'name',
-//         labelField: 'name',
-//         highlight: false,
-//         onType: refine,
-//         onBlur() {
-//           refine(this.getValue());
-//         },
-//         score() {
-//           return function() {
-//             return 1;
-//           };
-//         },
-//         onChange(value) {
-//           onSelectChange(value);
-//           refine(value);
-//         },
-//       });
-
-//       return;
-//     }
-
-//     const [select] = container.find('select');
-
-//     indices.forEach(index => {
-//       select.selectize.clearOptions();
-//       index.results.hits.forEach(h => select.selectize.addOption(h));
-//       select.selectize.refreshOptions(select.selectize.isOpen);
-//     });
-//   }
-// );
-
-// const suggestions = instantsearch({
-//   indexName: 'goml_DEMO',
-//   searchClient,
-// });
-
-// suggestions.addWidget(
-//   instantsearch.widgets.configure({
-//     hitsPerPage: 5,
-//   })
-// );
-
-// suggestions.addWidget(
-//   autocomplete({
-//     container: $('#autocomplete'),
-//     onSelectChange(value) {
-//       search.helper.setQuery(value).search();
-//     },
-//   })
-// );
-
-// const search = instantsearch({
-//   indexName: 'goml_DEMO',
-//   searchClient,
-// });
-
-// search.addWidget(
-//   instantsearch.widgets.configure({
-//     hitsPerPage: 10
-//   })
-// );
-
-// search.addWidget(
-//   instantsearch.widgets.hits({
-//     container: '#hits',
-//     templates: {
-//       item: `
-//         <div>
-//           <header class="hit-name">
-//             {{#helpers.highlight}}{ "attribute": "Program Name" }{{/helpers.highlight}}
-//           </header>
-//           <p class="hit-description">
-//             {{#helpers.highlight}}{ "attribute": "Area of Study" }{{/helpers.highlight}}
-//           </p>
-//           <p class="hit-description">
-//             {{#helpers.highlight}}{ "attribute": "Institution" }{{/helpers.highlight}}
-//           </p>
-//         </div>
-//       `,
-//     },
-//   })
-// );
-
-// suggestions.start();
-// search.start();
